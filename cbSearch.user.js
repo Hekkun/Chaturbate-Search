cd141c9bbd341010bfd05f1a8feaf9b0cb707bd1
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 15:23:01 -0700

    Update cbSearch.user.js

--- cbSearch.user.js
+++ cbSearch.user.js
@@ -96,4 +96,9 @@
 	
 	var context = new Bubbler(options);
 	
+	var button = document.createElement("Button");
+	button.innerHTML = "Title";
+	button.style = "top:0;right:0;position:absolute;z-index: 9999"
+	document.body.appendChild(button);
+	
 })();
