274120ac3750d7bd16894dec568973f1f5c563bb
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 14:21:41 -0700

    Update chsearch.js

--- chsearch.js
+++ chsearch.js
@@ -1,17 +1,18 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Chaturbate Search
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Search chatrubate models on camcaps.me
+// @author       hekkun
 // @match        https://chaturbate.com/*/
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/Hekkun/Chaturbate-Search/master/chsearch.js?token=AL3GA5DYRTVKUDD4UZWFOOS47V3Z6
+// @updateURL    https://raw.githubusercontent.com/Hekkun/Chaturbate-Search/master/chsearch.js?token=AL3GA5DYRTVKUDD4UZWFOOS47V3Z6
 // ==/UserScript==
 
 (function() {
-    'use strict';
-
-    // Your code here...
-		
-		alert("Test");
+	'use strict';
+	
+	// Your code here...
+	alert("Test");
 })();
