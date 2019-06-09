59c174b3ca583e186bfdd79cea6c8e8d3af13118
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 14:56:39 -0700

    Update cbSearch.user.js

--- cbSearch.user.js
+++ cbSearch.user.js
@@ -7,7 +7,7 @@
 // @match	https://chaturbate.com/*/
 // @exclude	https://chaturbate.com/tag/*
 // @exclude	https://chaturbate.com/*-cams/
-// @grant	none
+// @grant	GM_addStyle
 // @downloadURL	https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
 // @updateURL	https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
 // ==/UserScript==
@@ -16,4 +16,68 @@
 	'use strict';
 	
 	// Your code here...
+	
+	function Bubbler(items) {
+	    this.items = items;
+	    var body = document.getElementsByTagName('body')[0];
+	    var bubbler_wrapper = document.createElement('div');
+	    bubbler_wrapper.className = 'bubbler-wrapper';
+	    var menu_button = document.createElement('div');
+	    menu_button.className = 'bubbler-menu-loader';
+	    menu_button.innerHTML = '&#9776;';
+	    for (var i = this.items.length - 1; i >= 0; i--) {
+		var child = document.createElement('div');
+		child.className = 'bubbler-menu-item';
+		child.innerHTML = this.items[i].icon;
+		if (this.items[i].callback) {
+		    child.onclick = this.items[i].callback
+		}
+		if (this.items[i].display) {
+		    for (var key in this.items[i].display) {
+			if (!this.items[i].display.hasOwnProperty(key)) {
+			    continue
+			}
+			child.style[key] = this.items[i].display[key]
+		    }
+		}
+		var label_wrapper = document.createElement('label');
+		label_wrapper.className = 'bubbler-menu-item-label-wrapper';
+		var label = document.createElement('span');
+		label.className = 'bubbler-menu-item-label';
+		label.innerHTML = this.items[i].label;
+		label_wrapper.appendChild(label);
+		child.appendChild(label_wrapper);
+		bubbler_wrapper.appendChild(child)
+	    }
+	    bubbler_wrapper.appendChild(menu_button);
+	    body.appendChild(bubbler_wrapper)
+	}
+	
+	var zNode = document.createElement('div');
+	
+	var options =
+	[
+	  {
+	    icon:'1',
+	    label: 'Run alert function',
+	    callback: function() {
+	      alert('dasd');
+	    }
+	  },
+	  {
+	    icon:'<i class="fa fa-id-card" aria-hidden="true"></i>',
+	    label: 'Test Element 2'
+	  },
+	  {
+	    icon:'3',
+	    label: 'Test Element 3',
+	    display: {
+	      color: 'blue',
+	      background: 'red'
+	    }
+	  }
+	];
+	
+	var context = new Bubbler(options);
+	
 })();
