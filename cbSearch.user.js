728a9d7c7f067bf0ff0efd278a67342b349b7408
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 15:53:51 -0700

    Update cbSearch.user.js

--- cbSearch.user.js
+++ cbSearch.user.js
@@ -43,8 +43,25 @@
 	addCss('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css');
 	addScript('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js');
 	
+	const fabhtml = `
+		<div class="fixed-action-btn">
+		  <a class="btn-floating btn-large red">
+		    <i class="large material-icons">mode_edit</i>
+		  </a>
+		  <ul>
+		    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+		    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+		    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+		    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+		  </ul>
+		</div>
+	`;
+	document.body.appendChild(fabhtml);
 	
-	//var context = new Bubbler(options);
+	document.addEventListener('DOMContentLoaded', function() {
+	    var elems = document.querySelectorAll('.fixed-action-btn');
+	    var instances = M.FloatingActionButton.init(elems, options);
+	});
 	
 	var button = document.createElement("Button");
 	button.innerHTML = "Title";
