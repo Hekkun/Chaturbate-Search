c0e7379e71fdc0f4dd981d50a3b9c2c4ed4649d6
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 14:15:43 -0700

    Create chsearch.js

---
+++ chsearch.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://chaturbate.com/*/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+		
+		alert("Test");
+})();
