d4c25a287a480287e5444ff490a86a4471ee84a5
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 14:31:33 -0700

    Update cbSearch.user.js

--- cbSearch.user.js
+++ cbSearch.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name         Chaturbate Search
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Search for Chaturbate models on camcaps.me
-// @author       hekkun
-// @match        https://chaturbate.com/*/
-// @grant        none
-// @downloadURL  https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
-// @updateURL    https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
+// @name	Chaturbate Search
+// @namespace	http://tampermonkey.net/
+// @version	0.1
+// @description	Search for Chaturbate models on camcaps.me
+// @author	hekkun
+// @match	https://chaturbate.com/*/
+// @exclude	https://chaturbate.com/tag/*
+// @exclude	https://chaturbate.com/*-cams/
+// @grant	none
+// @downloadURL	https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
+// @updateURL	https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
 // ==/UserScript==
 
 (function() {
 	'use strict';
 	
 	// Your code here...
-	alert("Test");
 })();
