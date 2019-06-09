728db5acc13cffb31d29811987f541776b83eb83
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 15:24:29 -0700

    Update cbSearch.user.js

--- cbSearch.user.js
+++ cbSearch.user.js
@@ -98,7 +98,7 @@
 	
 	var button = document.createElement("Button");
 	button.innerHTML = "Title";
-	button.style = "top:0;right:0;position:absolute;z-index: 9999"
+	button.style = "top:100;right:100;position:absolute;z-index: 19999"
 	document.body.appendChild(button);
 	
 })();
