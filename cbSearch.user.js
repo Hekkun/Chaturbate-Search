67252ef3c0a9819084f8a91be49a64aa67df7f30
Hekkun <49700980+Hekkun@users.noreply.github.com>
2019-06-09 17:31:47 -0700

    Update cbSearch.user.js

--- cbSearch.user.js
+++ cbSearch.user.js
@@ -5,69 +5,57 @@
 // @description	Search for Chaturbate models on camcaps.me
 // @author	hekkun
 // @match	https://chaturbate.com/*/
+// @include	http://www.blank.org/
 // @exclude	https://chaturbate.com/tag/*
 // @exclude	https://chaturbate.com/*-cams/
 // @grant	GM_addStyle
+// @grant   GM_openInTab
 // @downloadURL	https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
 // @updateURL	https://github.com/Hekkun/Chaturbate-Search/raw/master/cbSearch.user.js
 // ==/UserScript==
 
 (function() {
 	'use strict';
-	
-	// Your code here...
 
-	
-	function addCss(fileName) {
+	// Reference: https://stackoverflow.com/questions/6480082/add-a-javascript-button-using-greasemonkey-or-tampermonkey
 
-	  var head = document.head;
-	  var link = document.createElement("link");
+    var zNode = document.createElement ('div');
+    zNode.innerHTML = '<button id="myButton" type="button">'
+        + 'camcaps.me</button>'
+    ;
+    zNode.setAttribute ('id', 'myContainer');
+    document.body.appendChild (zNode);
 
-	  link.rel = "stylesheet";
-	  link.href = fileName;
+    //--- Activate the newly added button.
+    document.getElementById ("myButton").addEventListener (
+        "click", ButtonClickAction, false
+    );
 
-	  head.appendChild(link);
-	}
-	
-	function addScript(fileName) {
 
-	  var head = document.head;
-	  var script = document.createElement("script");
 
-	  script.src = fileName
+    function ButtonClickAction (zEvent) {
+        var url = window.location.pathname;
+        url = url.replace(/^\/|\/$/g, '');
 
-	  head.appendChild(script);
-	}
+        var camcaps_url = "http://camcaps.me/search/" + url
 
-	
-	addCss('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css');
-	addScript('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js');
-	
-	const fabhtml = `
-		<div class="fixed-action-btn">
-		  <a class="btn-floating btn-large red">
-		    <i class="large material-icons">mode_edit</i>
-		  </a>
-		  <ul>
-		    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-		    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-		    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-		    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-		  </ul>
-		</div>
-	`;
-	var fab = document.createElement('div');
-	fab.innerHTML = fabhtml;
-	document.body.appendChild(fab);
-	
-	document.addEventListener('DOMContentLoaded', function() {
-	    var elems = document.querySelectorAll('.fixed-action-btn');
-	    var instances = M.FloatingActionButton.init(elems, options);
-	});
-	
-	var button = document.createElement("Button");
-	button.innerHTML = "Title";
-	button.style = "top:100px;right:100px;position:absolute;z-index: 19999"
-	document.body.appendChild(button);
-	
+        GM_openInTab(camcaps_url);
+    }
+
+    //--- Style our newly added elements using CSS.
+
+	GM_addStyle ( `
+	    #myContainer {
+		position:               fixed;
+		top:                    50px;
+		right:                  5px;
+		z-index:                19999;
+	    }
+	    #myButton {
+		cursor:                 pointer;
+	    }
+	` );
+
+
+
 })();
